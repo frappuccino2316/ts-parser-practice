877abb8464367ebfbf99dc230359bdafeaaabdab
frappuccino2316 <frappuccino2316@gmail.com>
2023-08-06 01:28:08 +0900

    出力結果を貼っておく

--- index.ts
+++ index.ts
@@ -8,3 +8,33 @@ const parsed = babelParser.parse(code, {
 
 console.log(code);
 console.log(parsed);
+
+// output of parsed
+// Node {
+//   type: 'File',
+//   start: 0,
+//   end: 65,
+//   loc: SourceLocation {
+//     start: Position { line: 1, column: 0, index: 0 },
+//     end: Position { line: 5, column: 0, index: 65 },
+//     filename: undefined,
+//     identifierName: undefined
+//   },
+//   errors: [],
+//   program: Node {
+//     type: 'Program',
+//     start: 0,
+//     end: 65,
+//     loc: SourceLocation {
+//       start: [Position],
+//       end: [Position],
+//       filename: undefined,
+//       identifierName: undefined
+//     },
+//     sourceType: 'module',
+//     interpreter: null,
+//     body: [ [Node], [Node], [Node] ],
+//     directives: []
+//   },
+//   comments: []
+// }
